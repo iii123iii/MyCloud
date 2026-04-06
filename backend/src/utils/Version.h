abac19fb8e0523c30049aa45adebb8cd266c234e
iii123iii <117964666+iii123iii@users.noreply.github.com>
2026-04-06 17:56:26 +0300

    Fix

--- backend/src/utils/Version.h
+++ backend/src/utils/Version.h
@@ -24,6 +24,6 @@
 // The tag on GitHub must match this string exactly (including the leading "v").
 //
 #ifndef MYCLOUD_VERSION
-#define MYCLOUD_VERSION     "v1.1.0"
+#define MYCLOUD_VERSION     "v1.1.1"
 #endif
 #define MYCLOUD_GITHUB_REPO "iii123iii/MyCloud"
